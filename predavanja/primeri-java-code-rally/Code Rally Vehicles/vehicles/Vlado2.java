43231d977329fdce790dbcd196dadfd303fe7cc3
Vladimir Filipovic <vladofilipovic@hotmail.com>
2019-03-02 12:56:15 +0100

    dodao

---
+++ predavanja/primeri-java-code-rally/Code Rally Vehicles/vehicles/Vlado2.java
@@ -0,0 +1,71 @@
+import com.ibm.coderally.api.DefaultCarAI;
+import com.ibm.coderally.entity.cars.Car;
+import com.ibm.coderally.entity.obstacle.Obstacle;
+import com.ibm.coderally.geo.CheckPoint;
+
+public class Vlado2 extends DefaultCarAI {
+
+	@Override
+	public void onCarCollision(Car car) {
+		// TODO:
+		// Provide custom logic or remove method
+		// for default implementation.
+	}
+
+	@Override
+	public void onCheckpointUpdated(CheckPoint cp) {
+		// TODO:
+		// Provide custom logic or remove method
+		// for default implementation.
+	}
+
+	@Override
+	public void onObstacleCollision(Obstacle obs) {
+		// TODO:
+		// Provide custom logic or remove method
+		// for default implementation.
+	}
+
+	@Override
+	public void onObstacleInProximity(Obstacle obs) {
+		// TODO:
+		// Provide custom logic or remove method
+		// for default implementation.
+	}
+
+	@Override
+	public void onOffTrack() {
+		// TODO:
+		// Provide custom logic or remove method
+		// for default implementation.
+	}
+
+	@Override
+	public void onOpponentInProximity(Car car) {
+		// TODO:
+		// Provide custom logic or remove method
+		// for default implementation.
+	}
+
+	@Override
+	public void onRaceStart() {
+		// TODO:
+		// Provide custom logic or remove method
+		// for default implementation.
+	}
+
+	@Override
+	public void onStalled() {
+		// TODO:
+		// Provide custom logic or remove method
+		// for default implementation.
+	}
+
+	@Override
+	public void onTimeStep() {
+		// TODO:
+		// Provide custom logic or remove method
+		// for default implementation.
+	}
+	
+}
